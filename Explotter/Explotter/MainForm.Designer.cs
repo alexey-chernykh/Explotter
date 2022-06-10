2c2f458c103d3125bd79053db1b42fc987378fa1
Oleksii_Chernykh <84220570+alexey-chernykh@users.noreply.github.com>
2022-06-10 12:32:56 +0300

    Explotter commit
    first commit

---
+++ Explotter/Explotter/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Explotter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.columnHeaderFileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderCreationTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderAttrib = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageListLarge = new System.Windows.Forms.ImageList(this.components);
+            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.toolStripDrives = new System.Windows.Forms.ToolStrip();
+            this.imageListTree = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.backToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.backToolStripMenuItem.Text = "Back";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(800, 401);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 24);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(800, 426);
+            this.toolStripContainer1.TabIndex = 1;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStripDrives);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewFiles);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 401);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageListTree;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(266, 401);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCollapse);
+            this.treeView1.AfterCollapse += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCollapse);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderFileName,
+            this.columnHeaderSize,
+            this.columnHeaderCreationTime,
+            this.columnHeaderAttrib});
+            this.listViewFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewFiles.FullRowSelect = true;
+            this.listViewFiles.GridLines = true;
+            this.listViewFiles.HideSelection = false;
+            this.listViewFiles.LargeImageList = this.imageListLarge;
+            this.listViewFiles.Location = new System.Drawing.Point(0, 0);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(530, 401);
+            this.listViewFiles.SmallImageList = this.imageListSmall;
+            this.listViewFiles.TabIndex = 0;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            this.listViewFiles.View = System.Windows.Forms.View.Details;
+            this.listViewFiles.DoubleClick += new System.EventHandler(this.listViewFiles_DoubleClick);
+            // 
+            // columnHeaderFileName
+            // 
+            this.columnHeaderFileName.Text = "File Name";
+            this.columnHeaderFileName.Width = 207;
+            // 
+            // columnHeaderSize
+            // 
+            this.columnHeaderSize.Text = "Size";
+            this.columnHeaderSize.Width = 97;
+            // 
+            // columnHeaderCreationTime
+            // 
+            this.columnHeaderCreationTime.Text = "Creation Time";
+            this.columnHeaderCreationTime.Width = 106;
+            // 
+            // columnHeaderAttrib
+            // 
+            this.columnHeaderAttrib.Text = "Attributes";
+            this.columnHeaderAttrib.Width = 116;
+            // 
+            // imageListLarge
+            // 
+            this.imageListLarge.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListLarge.ImageSize = new System.Drawing.Size(48, 48);
+            this.imageListLarge.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageListSmall
+            // 
+            this.imageListSmall.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListSmall.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // toolStripDrives
+            // 
+            this.toolStripDrives.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStripDrives.Location = new System.Drawing.Point(3, 0);
+            this.toolStripDrives.Name = "toolStripDrives";
+            this.toolStripDrives.Size = new System.Drawing.Size(111, 25);
+            this.toolStripDrives.TabIndex = 0;
+            // 
+            // imageListTree
+            // 
+            this.imageListTree.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListTree.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageListTree.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Explorer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listViewFiles;
+        private System.Windows.Forms.ColumnHeader columnHeaderFileName;
+        private System.Windows.Forms.ImageList imageListLarge;
+        private System.Windows.Forms.ImageList imageListSmall;
+        private System.Windows.Forms.ToolStrip toolStripDrives;
+        private System.Windows.Forms.ColumnHeader columnHeaderSize;
+        private System.Windows.Forms.ColumnHeader columnHeaderCreationTime;
+        private System.Windows.Forms.ColumnHeader columnHeaderAttrib;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageListTree;
+    }
+}
+
---
+++ Explotter/Explotter/MainForm.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explotter
+{
+    public partial class MainForm : Form
+    {
+        int count = 0;
+        string currentPath = Path.GetPathRoot(Environment.SystemDirectory);
+        List<string> previousPath = new List<string>();
+
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        private const int WM_DEVICECHANGE = 0x0219;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        [DllImport("Shell32.dll", EntryPoint = "ExtractAssociatedIcon")]
+        extern static IntPtr ExtractAssociatedIconAPI(IntPtr hInst, String iconPath, ref int piIcon);
+        [DllImport("Kernel32.dll")]
+        extern static IntPtr GetModuleHandle(String moduleName);
+        static Icon ExtractAssociatedIcon(String iconPath)
+        {
+            IntPtr moduleHandle = GetModuleHandle(null);
+            int piIcon = 0;
+            IntPtr hIcon = ExtractAssociatedIconAPI(moduleHandle, iconPath, ref piIcon);
+            return Icon.FromHandle(hIcon);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if(m.Msg == WM_DEVICECHANGE && m.WParam == (IntPtr)DBT_DEVICEARRIVAL || m.WParam == (IntPtr)DBT_DEVICEREMOVECOMPLETE)
+            {
+                FillDrives();
+            }
+            base.WndProc(ref m);
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            FillDrives();
+            FillFilePanel();
+
+            String my = "My Computer";
+            Image icon = Bitmap.FromFile("comp.ico");
+            imageListTree.Images.Add("/",icon);
+            imageListTree.Images.Add("/", icon);
+
+            TreeNode rootNode = new TreeNode(my, 0, 1);
+            rootNode.ImageKey = "/";
+            rootNode.Nodes.Add(new TreeNode("empty"));
+            treeView1.Nodes.Add(rootNode);
+        }
+
+        private void FillFilePanel()
+        {
+            listViewFiles.Items.Clear();
+            imageListLarge.Images.Clear();
+            imageListSmall.Images.Clear();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentPath);
+            foreach(DirectoryInfo dir in directoryInfo.GetDirectories())
+            {
+                var item = listViewFiles.Items.Add(dir.Name);
+                item.SubItems.Add("DIR");
+                item.SubItems.Add(dir.CreationTime.ToString("dd.MM.yyyy"));
+                item.SubItems.Add(dir.Attributes.ToString());
+                imageListSmall.Images.Add(dir.FullName, ExtractAssociatedIcon(dir.FullName));
+                imageListLarge.Images.Add(dir.FullName, ExtractAssociatedIcon(dir.FullName));
+                item.ImageKey = dir.FullName;
+            }
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                var item = listViewFiles.Items.Add(file.Name);
+                item.SubItems.Add(file.Length.ToString());
+                item.SubItems.Add(file.CreationTime.ToString("dd.MM.yyyy"));
+                item.SubItems.Add(file.Attributes.ToString());
+                imageListSmall.Images.Add(file.FullName, ExtractAssociatedIcon(file.FullName));
+                imageListLarge.Images.Add(file.FullName, ExtractAssociatedIcon(file.FullName));
+                item.ImageKey = file.FullName;
+                
+            }
+        }
+
+        private void FillDrives()
+        {
+            toolStripDrives.Items.Clear();
+            foreach (var drv in Directory.GetLogicalDrives())
+            {
+                var item = toolStripDrives.Items.Add(drv.Substring(0,2));
+                item.Image = ExtractAssociatedIcon(drv).ToBitmap();
+                item.Click += (o, e) =>
+                  {
+                      count = 0;
+                      previousPath.Add(currentPath);
+                      currentPath = drv;
+                      FillFilePanel();
+                  };
+            }
+        }
+
+        private void listViewFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems[0].SubItems[1].Text == "DIR")
+            {
+                ReloadListViewFiles(0);
+            }
+            else
+            {
+                String path = Path.Combine(currentPath, listViewFiles.SelectedItems[0].Text);
+                Process.Start(path);
+            }
+            
+        }
+
+        private void ReloadListViewFiles(int operation)
+        {
+            previousPath.Add(currentPath);
+            switch (operation)
+            {
+                case 0:
+                    currentPath = Path.Combine(currentPath, listViewFiles.SelectedItems[0].Text);
+                    break;
+                case 1:
+                    if (treeView1.SelectedNode.Text == "My Computer")
+                    {
+                        DoNothing();
+                    }
+                    else
+                    {
+                        currentPath = treeView1.SelectedNode.FullPath.Substring(12, treeView1.SelectedNode.FullPath.Length - 12);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+            
+            FillFilePanel();
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                currentPath = previousPath[(previousPath.Count - 1) - count];
+                count++;
+                FillFilePanel();
+            }
+            catch
+            {
+                DoNothing();
+            }
+        }
+
+        private void DoNothing()
+        {
+        }
+
+        private void treeView1_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+            TreeNode node = e.Node;
+
+            foreach (TreeNode n in node.Nodes)
+            {
+                imageListTree.Images.RemoveByKey(n.ImageKey);
+                imageListTree.Images.RemoveByKey(n.SelectedImageKey);
+            }
+
+            node.Nodes.Clear();
+            node.Nodes.Add("empty");
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
+        {
+
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            if (node ==treeView1.Nodes[0])
+            {
+                node.Nodes.Clear();
+                foreach (var drive in Directory.GetLogicalDrives())
+                {
+                    TreeNode currentNode = node.Nodes.Add(drive.Substring(0,2));
+                    currentNode.Nodes.Add("empty");
+                    currentNode.Tag = drive;
+                    currentNode.ImageKey = drive;
+                    imageListTree.Images.Add(drive, ExtractAssociatedIcon(drive));
+
+                }
+            }
+            else
+            {
+                node.Nodes.Clear();
+                foreach (var path in Directory.GetDirectories(node.Tag.ToString()))
+                {
+                    TreeNode currentNode = node.Nodes.Add(Path.GetFileName(path));
+                    currentNode.Nodes.Add("empty");
+                    currentNode.Tag = path;
+                    currentNode.ImageKey = path;
+                    imageListTree.Images.Add(path, ExtractAssociatedIcon(path));
+
+                }
+            }
+        }
+
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+            
+            ReloadListViewFiles(1);
+        }
+    }
+}
